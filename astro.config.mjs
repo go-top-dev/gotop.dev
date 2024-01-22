8a91b7dfedbaa68764029ea1b9f8e3f339306fdd
pengqun <pengqun1990@gmail.com>
2024-01-22 23:57:22 +0800

    Welcome to the universe, astronaut. 👩🏼‍🚀👨🏿‍🚀🧑‍🚀👩🏾‍🚀

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://gotop.dev"
+  site: "https://gotop.dev",
+  integrations: [preact()]
 });
